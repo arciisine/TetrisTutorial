22ee16ac90de03ae8b6b50477c56f77053e77bb0
Timothy Soehnlin <tim@eaiti.com>
2017-07-20 22:23:44 -0400

    Working on final pieces

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,11 +3,3 @@ import { getPiece } from "./piece";
 
 
 export const game = new Tetris();
-
-setInterval(function () {
-  if (!game.paused) {
-    if (!game.movePiece({ y: 1 })) {
-      game.nextPiece();
-    }
-  }
-}, 1000);
--- src/app/tetris.ts
+++ src/app/tetris.ts
@@ -16,6 +16,8 @@ export class Tetris {
   piece = getPiece();
   paused = false;
   board = buildBoard(BLOCKS_WIDE, BLOCKS_HIGH);
+  lastGravity: number = Date.now();
+  boardFilled: number[] = this.board.map(x => 0);
 
   constructor() {
     this.onKeyPress = this.onKeyPress.bind(this)
@@ -34,14 +36,30 @@ export class Tetris {
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
-  nextPiece() {
+  finishPiece() {
+    let removed = 0;
+    for (let i = BLOCKS_HIGH - 1; i >= 0; i--) {
+      if (this.boardFilled[i] === BLOCKS_WIDE) {
+        this.boardFilled.splice(i, 1);
+        this.board.splice(i, 1);
+        removed += 1;
+      }
+    }
+    for (let i = 0; i < removed; i++) {
+      this.boardFilled.unshift(0);
+      this.board.unshift(this.board[0].map(x => null));
+    }
     this.piece = getPiece();
+    if (!this.drawPiece(this.piece, 'test')) {
+      //Game over
+      document.write('GAME OVER');
+    }
   }
 
   drawPiece(piece: Piece, action: 'test'): boolean;
   drawPiece(piece: Piece, action: 'set' | 'clear'): void;
   drawPiece(piece: Piece, action: 'set' | 'clear' | 'test'): void | boolean {
-    const frame = piece.template.frames[piece.location.rotation % piece.template.frames.length];
+    const frame = piece.template.frames[Math.abs(piece.location.rotation % piece.template.frames.length)];
     const size = frame.length;
     const px = piece.location.x;
     const py = piece.location.y;
@@ -53,6 +71,7 @@ export class Tetris {
         for (let y = 0; y < size; y++) {
           if (frame[x][y]) {
             this.board[py + y][px + x] = add ? piece.style : null;
+            this.boardFilled[py + y] += add ? 1 : -1;
           }
         }
       }
@@ -77,6 +96,7 @@ export class Tetris {
 
     const ploc = this.piece.location;
     const loc = Object.assign({}, ploc);
+    let moved = false;
 
 
     if (location.x !== undefined) {
@@ -91,11 +111,12 @@ export class Tetris {
 
     if (!this.drawPiece(this.piece, 'test')) {
       this.piece.location = loc;
-      return false;
+    } else {
+      moved = true;
     }
 
     this.drawPiece(this.piece, 'set');
-    return true;
+    return moved;
   }
 
   onKeyPress(e: KeyboardEvent) {
@@ -103,8 +124,8 @@ export class Tetris {
     switch (key) {
       case KEY_CODES.LEFT: this.movePiece({ x: -1 }); break;
       case KEY_CODES.RIGHT: this.movePiece({ x: 1 }); break;
-      case KEY_CODES.UP: this.movePiece({ y: -1 }); break;
       case KEY_CODES.DOWN: this.movePiece({ y: 1 }); break;
+      case KEY_CODES.UP: this.movePiece({ rotation: -1 }); break;
       case KEY_CODES.SPACE: this.movePiece({ rotation: 1 }); break;
       case KEY_CODES.M:
         this.musicEnabled = !this.musicEnabled
@@ -129,6 +150,12 @@ export class Tetris {
 
   drawScreen() {
     this.clearScreen();
+    if ((Date.now() - this.lastGravity) > 1000) {
+      if (!this.movePiece({ y: 1 })) {
+        this.finishPiece();
+      }
+      this.lastGravity = Date.now();
+    }
     drawBoard(this.context, this.board);
     window.requestAnimationFrame(this.drawScreen);
   }
